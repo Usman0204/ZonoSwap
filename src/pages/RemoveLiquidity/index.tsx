90085f6278cb0f4e243381820652f65f9ab9569c
Usman Aslam Malik <usman.maliknu13@gmail.com>
2021-10-25 18:18:20 -0400

    changes

--- src/pages/RemoveLiquidity/index.tsx
+++ src/pages/RemoveLiquidity/index.tsx
@@ -121,7 +121,7 @@ export default function RemoveLiquidity({
       { name: 'verifyingContract', type: 'address' },
     ]
     const domain = {
-      name: 'Pancake LPs',
+      name: 'ZONO LP',
       version: '1',
       chainId,
       verifyingContract: pair.liquidityToken.address,
